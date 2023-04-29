a7523e3111dae2765d8267f228a82a3d9aeccd4e
ska-miya <ysk.miya.ska@gmail.com>
2023-04-29 21:18:25 +0900

    Create

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require("express");
+const app = express();
+//const coronaData = require("./coronaData.json");
+app.use(express.urlencoded({ extended: true}));
+const activities = require("./activities.json");
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function (req, res) {
+    console.log("req=", req.body);
+    fs.writeFile(__dirname + "/data.txt", req.body.activity, function(){
+        res.send("投稿完了");
+    });
+});
+
+app.post("/update", function(req, res) {
+    activities[0].activity = req.body.updatedActivity;
+    res.send(activities);
+});
+
+app.post("/delete", function(req, res) {
+    console.log(req.body);
+    activities.splice(req.body.number, 1);
+    res.send(activities);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+    console.log('Listening on ${port}');
+});
